e0acf98ccdf2d06a9fb8c8c0f095c4ccff12b891
Kristaps31 <kristaps.v@hotmail.co.uk>
2023-02-21 11:41:43 +0000

    this commit contains compleated branch 4.GET_api_reviews

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -27,14 +27,23 @@ describe("GET /api/categories", () => {
     })
 })
 describe("GET /api/reviews", ()=> {
-    test("200: responds with array of objects, each having following properties, owner: titlereview_id, category, review_img_url, created_at, votes, designer, comment_count", () => {
+    test("200: responds with array of objects, each having following properties, owner, title, review_id, category, review_img_url, created_at, votes, designer, comment_count", () => {
         return request (app)
         .get('/api/reviews')
         .expect(200)
         .then(({ body }) => {
             const { reviews } = body;
             expect(reviews).toHaveLength(13);
-            // reviews.forEach(review)
+            reviews.forEach(review => {
+                expect(review).toHaveProperty("title", expect.any(String));
+                expect(review).toHaveProperty("designer", expect.any(String));
+                expect(review).toHaveProperty("owner", expect.any(String));
+                expect(review).toHaveProperty("review_img_url", expect.any(String));
+                expect(review).toHaveProperty("review_body", expect.any(String));
+                expect(review).toHaveProperty("category", expect.any(String));
+                expect(review).toHaveProperty("created_at", expect.any(String));
+                expect(review).toHaveProperty("votes", expect.any(Number));
+            })
         });
     })
 })
