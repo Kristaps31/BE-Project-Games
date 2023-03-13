bc1d27e1959a39b3fb55549eb6d43ed39cefa4b2
Kristaps31 <kristaps.v@hotmail.co.uk>
2023-03-13 14:33:51 +0000

    updated connection file

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,6 @@ require('dotenv').config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
-
 const config =
   ENV === 'production'
     ? {
@@ -17,5 +16,5 @@ const config =
       }
     : {}; 
 
-module.exports = new Pool(config);
 
+module.exports = new Pool(config);
