01dd60bf84aaf997296aae92604bc537b161ec94
Kristaps31 <kristaps.v@hotmail.co.uk>
2023-03-14 14:35:25 +0000

    modified review file

--- models/games.models.js
+++ models/games.models.js
@@ -28,7 +28,7 @@ exports.selectReviews = () => {
     FROM reviews
     LEFT JOIN comments ON reviews.review_id = comments.review_id
     GROUP BY 
-    reviews.owner, reviews.title, reviews.review_id, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.designer
+    reviews.owner, reviews.title, reviews.review_id, reviews.category, reviews.review_img_url, reviews.created_at, reviews.votes, reviews.review_body, reviews.designer
     ORDER BY created_at DESC;
     `
     )
